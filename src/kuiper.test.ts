0f656907046181655d541dfa91c235ca4b881e52
sizumita <github@smdr.io>
2022-11-23 01:40:26 +0900

    Add tests

--- src/kuiper.test.ts
+++ src/kuiper.test.ts
@@ -10,3 +10,20 @@ test("Test post", async () => {
     const response = await kuiper.post("https://httpbin.org/post", {key: "value"})
     expect(response.ok).toBe(true)
 })
+
+test("Test post formData", async () => {
+    const data = new FormData()
+    data.set("A", "B")
+    const response = await kuiper.post("https://httpbin.org/post", data)
+    const result = await response.json<{form: {A: "B"}}>()
+    expect(response.ok).toBe(true)
+    expect(result.form.A).toBe("B")
+})
+
+test("Test post Blob", async () => {
+    const blob = new Blob(["test"])
+    const response = await kuiper.post("https://httpbin.org/post", blob)
+    const result = await response.json<{data: "test"}>()
+    expect(response.ok).toBe(true)
+    expect(result.data).toBe("test")
+})
