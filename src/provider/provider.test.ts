4644c62fc354b913ff3ace2e1c27bfd30400e9e0
sizumita <github@smdr.io>
2022-11-24 21:28:14 +0900

    Add tests

--- src/provider/provider.test.ts
+++ src/provider/provider.test.ts
@@ -2,14 +2,17 @@ import type {Method} from "../kuiper"
 import type {Task, Parameters} from "./index"
 import { Provider, Service} from "./index"
 
+
 enum ApiRoutes {
     testGet = "/get",
     testPost = "/post",
+    testPut = "/put"
 }
 
 type ApiTasks =
     Task<ApiRoutes.testGet, null> |
-    Task<ApiRoutes.testPost, {key: string}>
+    Task<ApiRoutes.testPost, {key: string}> |
+    Task<ApiRoutes.testPut, string>
 
 
 class ApiService extends Service<ApiRoutes, ApiTasks> {
@@ -25,6 +28,8 @@ class ApiService extends Service<ApiRoutes, ApiTasks> {
                 return "GET"
             case ApiRoutes.testPost:
                 return "POST"
+            case ApiRoutes.testPut:
+                return "PUT"
         }
     }
 
@@ -33,6 +38,7 @@ class ApiService extends Service<ApiRoutes, ApiTasks> {
             case ApiRoutes.testGet:
                 return {}
             case ApiRoutes.testPost:
+            case ApiRoutes.testPut:
                 return {body: data}
         }
     }
@@ -43,4 +49,20 @@ test("Test provider `testGet`", async () => {
     const provider = new Provider<ApiRoutes, ApiTasks>(new ApiService())
     const response = await provider.request([ApiRoutes.testGet, null])
     expect(response.ok).toBe(true)
+})
+
+test("Test provider `testPost`", async () => {
+    const provider = new Provider<ApiRoutes, ApiTasks>(new ApiService())
+    const response = await provider.request([ApiRoutes.testPost, {key: "value"}])
+    expect(response.ok).toBe(true)
+    const result = await response.json<{data: "{\"key\":\"value\"}"}>()
+    expect(result.data).toBe("{\"key\":\"value\"}")
+})
+
+test("Test provider `testPut`", async () => {
+    const provider = new Provider<ApiRoutes, ApiTasks>(new ApiService())
+    const response = await provider.request([ApiRoutes.testPut, "test"])
+    expect(response.ok).toBe(true)
+    const result = await response.json<{data: "test"}>()
+    expect(result.data).toBe("test")
 })
